1542a79df79ae06b365cb2ca0a59a2c3397cfd6e
Nicollas <nicollasramires@gmail.com>
2022-10-18 22:29:23 -0300

    finalizando rotas

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express, { query, request, response } from 'express'
+import cors from 'cors'
+
 import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
 
 const app = express()
+app.use(express.json())
+app.use(cors({
+   // origin: 'http://'
+}))
+
 const prisma = new PrismaClient({
     log: ['query'] // serve para logar no console o comando SQL executado pelas rotas
 })
@@ -20,8 +29,26 @@ app.get('/games', async (request, response) => {
     return response.json(games);
 });
 
-app.post('/ads', (request, response) => {
-    return response.status(201).json([]);
+app.post('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
+    const body: any = request.body;
+
+    // implementar validações: utilizar biblioteca zod
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourStringToMinutes(body.hourStart),
+            hourEnd: convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+
+    return response.status(201).json(ad);
 });
 
 app.get('/games/:id/ads', async (request, response) => {
@@ -48,7 +75,9 @@ app.get('/games/:id/ads', async (request, response) => {
     return response.json(ads.map(ad => {
         return{
             ...ad,
-            weekDays: ad.weekDays.split(',')
+            weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd : convertMinutesToHourString(ad.hourEnd)
         }
     }))
 })
